c31ca8548a00da81dd6e9e4f06cd214862fa3081
devbryan0225 <devbryan0225@users.noreply.github.com>
2020-12-06 23:35:12 +0800

    Startup files

---
+++ dependencies_setup.ps1
@@ -0,0 +1,9 @@
+# Delete this file after setup
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+npm install tailwindcss --save
+npm install autoprefixer --save
+npm install postcss-import --save
+npm install postcss-cli --save
+
+# build script for sass
+# sass css/style.sass:css/style.css
